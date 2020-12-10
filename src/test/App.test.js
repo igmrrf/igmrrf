055ac0df2b5791158a852b98eaaa3f4ff427a23d
igmrrf <francis.igbiriki.fi@gmail.com>
2020-12-11 00:36:06 +0100

    Test Setup

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,8 +1,44 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
-test('renders learn react link', () => {
+test('renders text in header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerText = screen.getByText(/t-l-d-o/i);
+
+  expect(headerText).toBeInTheDocument();
+});
+
+test('renders text in about', () => {
+  render(<App />);
+  const aboutText = screen.getByText(/about/i);
+
+  expect(aboutText).toBeInTheDocument();
+});
+
+test('renders text in stack', () => {
+  render(<App />);
+  const stackText = screen.getByText(/stack/i);
+
+  expect(stackText).toBeInTheDocument();
+});
+
+test('renders text in projects', () => {
+  render(<App />);
+  const projectsText = screen.getByText(/projects/i);
+
+  expect(projectsText).toBeInTheDocument();
+});
+
+test('renders text in contact', () => {
+  render(<App />);
+  const contactText = screen.getByText(/contact/i);
+
+  expect(contactText).toBeInTheDocument();
+});
+
+test('renders text in footer', () => {
+  render(<App />);
+  const footerText = screen.getByText(/copyright/i);
+
+  expect(footerText).toBeInTheDocument();
 });
