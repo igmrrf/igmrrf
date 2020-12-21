ffb7f3e465ff44167ae7f8b4f339ae2c68a1348e
igmrrf <francis.igbiriki.fi@gmail.com>
2020-12-22 00:09:57 +0100

    Update

--- src/styled/theme.jsx
+++ src/styled/theme.jsx
@@ -13,6 +13,14 @@ const typography = {
   h5: '2em',
   h6: '2em',
 };
+const fonts = {
+  h1: '2em',
+  h2: '2em',
+  h3: '2em',
+  h4: '2em',
+  h5: '2em',
+  h6: '2em',
+};
 
 const theme = {
   main: '#00FF66',
@@ -37,6 +45,7 @@ export const lightTheme = {
   yan: '#000C18',
   typography,
   effects,
+  fonts,
 };
 
 export default theme;
