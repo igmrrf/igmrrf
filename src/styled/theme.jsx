56861614144229ede715dfe71f00d8eaba289e4c
igmrrf <francis.igbiriki@gmail.com>
2021-01-07 21:03:24 +0100

    After Figma Design Themeing

--- src/styled/theme.jsx
+++ src/styled/theme.jsx
@@ -1,4 +1,4 @@
-import { rotate, pulse, colorPulse } from './effects';
+import { rotate, pulse, colorPulse } from "./effects";
 
 const effects = {
   rotate,
@@ -6,46 +6,39 @@ const effects = {
   colorPulse,
 };
 const typography = {
-  h1: '2em',
-  h2: '2em',
-  h3: '2em',
-  h4: '2em',
-  h5: '2em',
-  h6: '2em',
+  h1: "2em",
+  h2: "2em",
+  h3: "2em",
+  h4: "2em",
+  h5: "2em",
+  h6: "2em",
 };
-const fonts = {
-  h1: '2em',
-  h2: '2em',
-  h3: '2em',
-  h4: '2em',
-  h5: '2em',
-  h6: '2em',
+export const dark = {
+  ichi: "#000C18",
+  ni: "#2C7D54",
+  san: "#0DFC85",
+  yan: "white",
+};
+
+export const light = {
+  ichi: "white",
+  ni: "#0DFC85",
+  san: "#2C7D54",
+  yan: "#000C18",
 };
 
 const theme = {
-  main: '#00FF66',
-  dark: 'darkblue',
-  light: 'lightblue',
-  typography,
-  effects,
-};
-
-export const darkTheme = {
-  ichi: '#000C18',
-  ni: '#2C7D54',
-  san: '#0DFC85',
-  yan: 'white',
-  typography,
-  effects,
-};
-export const lightTheme = {
-  ichi: 'white',
-  ni: '#0DFC85',
-  san: '#2C7D54',
-  yan: '#000C18',
+  ichi: "#0DFC85",
+  ni: "#800080",
+  san: "#FFFFFF",
+  yan: "#000C18",
+  dark,
+  light,
   typography,
   effects,
   fonts,
+  typography,
+  effects,
 };
 
 export default theme;
