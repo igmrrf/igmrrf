69e659dd00db5775f02168ade86c6abd5b82b680
Igbiriki Francis Richard <francis.igbiriki@gmail.com>
2021-08-08 17:55:06 +0100

    Modifying the analytics uploader file

--- src/helpers/analytics.js
+++ src/helpers/analytics.js
@@ -1,6 +1,7 @@
 export function sendAnalytics(metric) {
   const body = JSON.stringify(metric);
   const url = 'https://example.com/analytics';
+  // a url that holds or saves analytics 
 
   if (navigator.sendBeacon) {
     navigator.sendBeacon(url, body);
